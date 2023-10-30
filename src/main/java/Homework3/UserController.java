bd6e19577dbbac315a0e378eeeb15aa8afe58892
Anton Gargalyk <gargalyk1994@gmail.com>
2023-10-30 13:05:39 +0900

    Make homework 3 again

---
+++ src/main/java/Homework3/UserController.java
@@ -0,0 +1,42 @@
+package Homework3;
+
+
+import java.util.List;
+
+public class UserController {
+
+    private final DataService dataService = new DataService();
+    private final StudentGroupService studentGroupService = new StudentGroupService();
+    private final UserView userView = new UserView();
+    private StreamService streamService;
+
+    public void createStudent(String firstName, String secondName, String patronymic, int dateOfBirth){
+        dataService.create(firstName, secondName, patronymic, dateOfBirth);
+        List<User> userList = dataService.getAll();
+        userView.sendOnConsole(userList);
+    }
+
+    public void createStudentGroup(Teacher teacher, List<Student> students){
+        studentGroupService.createStudentGroup(teacher, students);
+    }
+
+    public Student getStudentInStudentGroup(String firstName, String secondName){
+        return studentGroupService.getStudentFromStudentGroup(firstName, secondName);
+    }
+
+    public List<Student> getSortedListStudentFromStudentGroup(){
+        return studentGroupService.getSortedStudentGroup();
+    }
+
+    public List<Student> getSortedListByFIOStudentFromStudentGroup(){
+        return studentGroupService.getSortedByFIOStudentGroup();
+    }
+
+    public UserController(StreamService streamService) {
+        this.streamService = streamService;
+    }
+
+    public List<Stream> sortListStream(List<Stream> streams){
+        return streamService.sortStreams(streams);
+    }
+}
